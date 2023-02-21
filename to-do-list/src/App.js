4889c01b58cb03d178e0e634563ba5b29691b3c0
hitu <1okqzoo09@gmail.com>
2023-02-21 19:29:33 +0900

    1st commit

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import TodoBoard from './components/TodoBoard';
 
+
+//input창, button 생성
+//input에 값을 입력하면 아이템이 추가된다.
+//삭제기능 해보기!!
 function App() {
+  //input값을 저장하는 usestate
+  const [inputValue, setInputValue] = useState('')
+
+  //값을 저장 => 배열이 가장 좋다!
+  const [todoList, setTodoList] = useState('')
+  
+  //버튼을 클릭할때마다 ...기존값을 냅두고 신규값 업뎃해줘
+  const addItem=()=>{
+    setTodoList([...todoList, inputValue])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+        {/*
+           onChange => input창의 값이 변할때마다 알려줌 
+           event.target.value는 input에 입력한 값이 있음
+           이 값을 저장하기 위해선 setInputValue 사용(state)
+           초기값 inputValue 지정(state)
+          */}
+        <input type="text" value={inputValue} onChange={(event)=>setInputValue(event.target.value)}/>
+        <button onClick={addItem}>추가</button>
+
+          {/* components todoboard에 값을 전달*/}
+        <TodoBoard todoList={todoList}/>
+    </main>
   );
 }
 
---
+++ to-do-list/src/components/TodoBoard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TodoItem from './TodoItem';
+
+function TodoBoard(props){
+    //console.log(props.todoList)
+    return(
+        <div>
+            <h1>To do List</h1>
+            {props.todoList.map((item)=><TodoItem item={item}/>)}
+            {/* {props.todoList.map((item)=><TodoItem item={item}/>)} */}
+        </div>
+    )
+}
+
+export default TodoBoard
